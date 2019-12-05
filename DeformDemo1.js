22506f6b93a0589cad23eb61c86adb7fa1f9b762
James Lockard-Nobile <jlockard@iastate.edu>
2019-12-04 20:03:40 -0600

    broken

--- DeformDemo1.js
+++ DeformDemo1.js
@@ -17,31 +17,121 @@ var ascpectRatio = window.innerWidth / window.innerHeight;
 var nearClip = 0.1;
 var farClip = 1000;
 var camera = new THREE.PerspectiveCamera(fov, ascpectRatio, nearClip, farClip);
-camera.position.set(0,0,50);
+camera.position.set(20,20,50);
 camera.rotation.x = -Math.PI / 2;
 camera.lookAt(new THREE.Vector3(0,0,0));
 
-var skyLight = new THREE.HemisphereLight( 0xffffbb, 0x080820, 0.15 );
+// var light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 0.15 );
 
 const geometry = new THREE.BoxGeometry(20, 20, 20, 20, 20, 20); 
-const material = new THREE.MeshNormalMaterial({ wireframe: true }); 
+const material = new THREE.MeshNormalMaterial({ color: "0x010101" }); 
 const cube = new THREE.Mesh(geometry, material); 
 
-const quaternion = new THREE.Quaternion(); 
-quaternion.setFromAxisAngle( 
-  new THREE.Vector3(0, 1, 0), 
-  Math.PI / 2 
-);  
-const vector = new THREE.Vector3(1, 0, 0); 
-vector.applyQuaternion(quaternion);
-
 // Add pieces to the scene
-scene.add(skyLight, cube);
+scene.add(cube);
+
+// let twist = {
+//     amount: 100,
+//     axis: y,
+//     direction: new THREE.Vector3(0, 1, 0),
+//     position: geometry.vertices,
+// }
 
 // Animate the scene
+let twist = {
+    amount: 0,
+    axis: 'x',
+}
 animate();
 
 function animate() {
+    twistObj( geometry );
     requestAnimationFrame( animate );
     renderer.render( scene, camera );
+}
+
+function twistObj(geometry) {
+    const quaternion = new THREE.Quaternion();
+    let position, direction = '';
+
+    for (let i = 0; i < geometry.vertices.length; i++) {
+        twist.axis == 'x' ? (direction = new THREE.Vector3(1, 0, 0), position = geometry.vertices[i].x):
+        twist.axis == 'y' ? (direction = new THREE.Vector3(0, 1, 0), position = geometry.vertices[i].y):
+        (direction = new THREE.Vector3(0, 0, 1), position = geometry.vertices[i].z);
+        
+        quaternion.setFromAxisAngle(
+            direction, 
+            (Math.PI / 180) * (position / twist.amount)
+        );
+
+        geometry.vertices[i].applyQuaternion(quaternion);
+    }
+
+    // tells Three.js to re-render this mesh
+    geometry.verticesNeedUpdate = true;
+}
+
+//translate keypress events to strings
+//from http://javascript.info/tutorial/keyboard-events
+function getChar(event) {
+    if (event.which == null) {
+     return String.fromCharCode(event.keyCode) // IE
+    } else if (event.which!=0 && event.charCode!=0) {
+     return String.fromCharCode(event.which)   // the rest
+    } else {
+     return null // special key
+    }
+}
+
+function handleKeyPress(event)
+{
+    var ch = getChar(event);
+
+	switch(ch) {
+	case 'x':
+        twist.axis = 'x'
+        break;
+	case 'y':
+		twist.axis = 'y'
+        break;
+	case 'z':
+        twist.axis = 'z'
+        break;
+	case 'a':
+        twist.amount -= 1
+		break;
+	case 'A':
+		twist.amount += 1
+		break;
+	// case 'f':
+	// 	camera.position.y -= 0.1
+	// 	break;
+    // case 'i':
+    //     camera.rotation.x += 0.1
+    //     break;
+    // case 'k':
+    //     camera.rotation.x -= 0.1
+    //     break;
+    // case 'j':
+    //     camera.rotation.y += 0.1
+    //     break;
+    // case 'l':
+    //     camera.rotation.y -= 0.1
+    //     break;
+    // case 'O':
+    //     camera.lookAt(new THREE.Vector3(0,0,0));
+    //     break;
+    // case 'p':
+    //     camera.position.set(0,0.5,0);
+    //     break;
+    // case 'W':
+    //     camera.fov -= 1
+    //     // camera.updateProjectionMatrix();
+    //     break;
+    // case 'S':
+    //     camera.fov += 1
+    //     // camera.updateProjectionMatrix();
+    //     break;
+    }
+    // camera.updateProjectionMatrix();
 }
